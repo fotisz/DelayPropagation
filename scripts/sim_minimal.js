bc638b95b6da498f1f3fd4549de1c216d947c77c
fotisz <fotisz@users.noreply.github.com>
2017-01-11 15:53:34 +0000

    Update sim_minimal.js

--- scripts/sim_minimal.js
+++ scripts/sim_minimal.js
@@ -43,7 +43,7 @@
 var margin = {top: 0, right: 20, bottom: 0, left: 20},
     width = 400 - margin.left - margin.right,
     height = 500 - margin.top - margin.bottom,
-    radius = (width-80)/2,
+    radius = (width-100)/2,
     center = {x: width/2, y: height/2},
     durPerm = 45,
     dur = 45;
