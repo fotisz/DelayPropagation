c60d699a4ebb5714018e60aab967de7e15e6c0f2
fotisz <fotisz@users.noreply.github.com>
2017-03-17 14:31:40 +0000

    Update

--- scripts/sim_minimal_b.js
+++ scripts/sim_minimal_b.js
@@ -91,16 +91,16 @@ var toRads = 2*Math.PI;
       
 var y = d3.scale.linear()
 	.domain([-7,1.5])
-	.range([0,radius + 55]) 		/* acceleration and deceleration */
+	.range([0,radius + 45]) 		/* acceleration and deceleration */
 	.clamp(true);
 
-var offset = 40;    				/* train icon position */
+var offset = 25;    				/* train icon position */
 
-var interiorGap = 45; 				/* acceleration and deceleration distance */
+var interiorGap = 30; 				/* acceleration and deceleration distance */
 
 var roadMaker = d3.svg.arc()
-	.innerRadius(radius-20)			/* road width */
-  	.outerRadius(radius+20)
+	.innerRadius(radius-15)			/* road width */
+  	.outerRadius(radius+15)
   	.startAngle(0)
   	.endAngle(2*Math.PI);
 
@@ -127,10 +127,10 @@ var arcInner = d3.svg.arc()
       .innerRadius(radius - 30)
       .outerRadius(radius + 30)
       .startAngle(function(d){
-      return (-0.5*(d.gap + offset)/numPatches*0.8 + 0.005) * toRads;
+      return (-0.5*(d.gap + offset)/numPatches*0.8 + 0.002) * toRads;
       })
       .endAngle(function(d){
-      return (0.5*(d.gap - offset)/numPatches*0.8 - 0.005) * toRads;
+      return (0.5*(d.gap - offset)/numPatches*0.8 - 0.002) * toRads;
       });
 
 //=============DRAW SVG AND ROAD===============
@@ -243,7 +243,7 @@ var car = gCar.selectAll('cars')
 		.call(sticker)
 			.attr({
 				class: "g-sticker",
-				transform: "translate(0," + (-radius + 5 ) +") scale(.4, 0.4) rotate(180)",
+				transform: "translate(0," + (-radius + 15 ) +") scale(.7, 0.7) rotate(180)", /* train icon */
 				fill: function(d,i){ return color(d.v); },
 			});
 
