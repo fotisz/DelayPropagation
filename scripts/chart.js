a20034676a0f8e8af6806c9a0c33c6f16a1d7b6b
fotisz <fotisz@users.noreply.github.com>
2017-04-07 15:34:28 +0100

    Update chart.js

--- scripts/chart.js
+++ scripts/chart.js
@@ -3,7 +3,7 @@
 var sticker = d3.sticker("#car");
 
 var numCars = 10,         /* # of trains  */
-    t = .9,               /* chart time scale */  
+    t = .5,               /* chart time scale */  
     numPatches = 500,
     timeScale = 100;
 
@@ -11,7 +11,7 @@ var numCars = 10,         /* # of trains  */
 var vo = 40,              /* driving parameters */
     vd = vo*0.7,
     lag = 4,
-    c = 0.36;
+    c = 0.5;
 
 var cars = d3.range(numCars).map(function(d,i){
     var x = i*170;
