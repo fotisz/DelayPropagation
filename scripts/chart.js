0c09e5f900317a258c1f2cf550181320849b4535
fotisz <fotisz@users.noreply.github.com>
2017-04-07 15:47:09 +0100

    Update chart.js

--- scripts/chart.js
+++ scripts/chart.js
@@ -9,7 +9,7 @@ var numCars = 10,         /* # of trains  */
 
 // Driving Parameters
 var vo = 40,              /* driving parameters */
-    vd = vo*0.3,
+    vd = vo*0.4,
     lag = 4,
     c = 0.36;
 
