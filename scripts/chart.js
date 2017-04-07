61541666c096d3885ce7c9417847db201bb28a51
fotisz <fotisz@users.noreply.github.com>
2017-04-07 15:44:31 +0100

    Update chart.js

--- scripts/chart.js
+++ scripts/chart.js
@@ -3,7 +3,7 @@
 var sticker = d3.sticker("#car");
 
 var numCars = 10,         /* # of trains  */
-    t = .4,               /* chart time scale */  
+    t = .6,               /* chart time scale */  
     numPatches = 500,
     timeScale = 100;
 
